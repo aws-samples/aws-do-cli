5ff07de430f7546a6921e6bf7526f6c0521a6e5f
iankouls-aws <iankouls@amazon.com>
2022-11-16 01:06:07 +0000

    Automatically determine region and ECR repo, push with single command

--- push.sh
+++ push.sh
@@ -7,5 +7,16 @@
 
 source .env
 
+# Create registry if needed
+REGISTRY_COUNT=$(aws ecr describe-repositories | grep ${IMAGE} | wc -l)
+if [ "$REGISTRY_COUNT" == "0" ]; then
+        aws ecr create-repository --repository-name ${IMAGE}
+fi
+
+# Login to registry
+echo "Logging in to $REGISTRY ..."
+aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY
+
+# Push image to registry
 docker image push ${REGISTRY}${IMAGE}${TAG}
 
